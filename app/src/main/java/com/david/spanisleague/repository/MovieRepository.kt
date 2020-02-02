d30fe1ee73b87e043f53ae4532c1d7a46a68c025
virtualassembler <virtualassembler@gmail.com>
2020-02-01 22:23:43 -0500

    retrofit initial commit

---
+++ app/src/main/java/com/david/spanisleague/repository/MovieRepository.kt
@@ -0,0 +1,57 @@
+package com.david.spanisleague.repository
+
+import android.content.Context
+import android.util.Log
+import com.david.spanisleague.R
+import com.david.spanisleague.api.ApiService
+import com.david.spanisleague.model.MovieDao
+import com.david.spanisleague.model.MovieResponse
+import com.david.spanisleague.model.MovieReview
+import com.david.spanisleague.model.MovieReviewDatabase
+import com.david.spanisleague.utils.API_KEY
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+/**
+ * MovieRepository
+ *
+ * This Class returns the data to the MainActivity
+ * a requestMovieReviewList method is created and asks if there is access to the internet performs a callback.enqueue,
+ * through retrofit and populates the bd implemented with room
+ *
+ * @author juan.rendon
+ */
+class MovieRepository(private val context: Context) {
+
+    private val apiService = ApiService.instance
+    private val movieDatabase: MovieDao get() = MovieReviewDatabase.getMovieDatabase(context).getMovieDAO()
+
+    fun requestMovieReviewList(): List<MovieReview> {
+        apiService.getMovieReviewListFromInternet(API_KEY).enqueue(object : Callback<MovieResponse> {
+            override fun onResponse(callMovieResponse: Call<MovieResponse>, response: Response<MovieResponse>) {
+                when (response.code()) {
+                    200 -> insertMovieReviewListIntoDatabase(response)
+                    else -> Log.e(context.getString(R.string.error_tag), context.getString(R.string.error_response_code_different_to_200))
+                }
+            }
+
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                Log.e(context.getString(R.string.error_tag), t.printStackTrace().toString())
+            }
+        })
+        return getMovieReviewList()
+    }
+
+    private fun insertMovieReviewListIntoDatabase(response: Response<MovieResponse>) {
+        if (response.body() != null) {
+            for (movieReview: MovieReview in response.body()!!.results) {
+                movieDatabase.insertMovieReview(movieReview)
+            }
+        }
+    }
+
+    fun getMovieReviewList(): List<MovieReview> {
+        return MovieReviewDatabase.getMovieDatabase(context).getMovieDAO().getMovieReviewList()
+    }
+}
